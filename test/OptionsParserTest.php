84544c9dc84de16b9b31c2d6cce272e25ed154c6
Richard Harrison <rharrison@podfather.com>
2018-08-13 00:00:19 +0100

    Worked with unit test

--- test/OptionsParserTest.php
+++ test/OptionsParserTest.php
@@ -1,5 +1,13 @@
 <?php
 
-class OptionsParserTest extends PHPUnit_Framework_TestCase {
+include_once __DIR__ . '/../src/OptionsParser.php';
+
+class OptionsParserTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testIt()
+    {
+        $fixture = new OptionsParser();
+    }
 
 }
